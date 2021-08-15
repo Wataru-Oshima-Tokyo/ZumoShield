1adb25f91c7f49cff116b98d8be2a7b30315b039
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-08-15 16:46:48 -0700

    Update ZumoShieldEncoders.cpp

--- lib/ZumoShield/ZumoShieldEncoders.cpp
+++ lib/ZumoShield/ZumoShieldEncoders.cpp
@@ -1,29 +1,31 @@
 // Copyright Pololu Corporation.  For more information, see http://www.pololu.com/
 
 #include <ZumoShieldEncoders.h>
-#include <FastGPIO.h>
-#include <avr/interrupt.h>
-#include <Arduino.h>
+// #include <FastGPIO.h>
+// #include <avr/interrupt.h>
+// #include <Arduino.h>
 
-#define LEFT_XOR   8
-#define LEFT_B     IO_E2
-#define RIGHT_XOR  7
-#define RIGHT_B    23
+// #define LEFT_XOR   8
+// #define LEFT_B     IO_E2
+// #define RIGHT_XOR  7
+// #define RIGHT_B    23
 #define RIGHT_CNT_FLAG 0x0001
 #define LEFT_CNT_FLAG  0x0002
 
-static volatile bool lastLeftA;
-static volatile bool lastLeftB;
-static volatile bool lastRightA;
-static volatile bool lastRightB;
+// static volatile bool lastLeftA;
+// static volatile bool lastLeftB;
+// static volatile bool lastRightA;
+// static volatile bool lastRightB;
 
-static volatile bool errorLeft;
-static volatile bool errorRight;
+// static volatile bool errorLeft;
+// static volatile bool errorRight;
 
 // These count variables are uint16_t instead of int16_t because
 // signed integer overflow is undefined behavior in C++.
 static volatile uint16_t countLeft;
 static volatile uint16_t countRight;
+int countLeft=0;
+int countRight=0;
 int flag = 0;
 
 ISR(TIMER1_CAPT_vect)
@@ -66,17 +68,17 @@ ISR(TIMER3_CAPT_vect)
 
 void ZumoShieldEncoders::init2()
 {
-    // Set the pins as pulled-up inputs.
-    FastGPIO::Pin<LEFT_XOR>::setInputPulledUp();
-    FastGPIO::Pin<LEFT_B>::setInputPulledUp();
-    FastGPIO::Pin<RIGHT_XOR>::setInputPulledUp();
-    FastGPIO::Pin<RIGHT_B>::setInputPulledUp();
+//     // Set the pins as pulled-up inputs.
+//     FastGPIO::Pin<LEFT_XOR>::setInputPulledUp();
+//     FastGPIO::Pin<LEFT_B>::setInputPulledUp();
+//     FastGPIO::Pin<RIGHT_XOR>::setInputPulledUp();
+//     FastGPIO::Pin<RIGHT_B>::setInputPulledUp();
 
-    // Enable pin-change interrupt on PB4 for left encoder, and disable other
-    // pin-change interrupts.
-    PCICR = (1 << PCIE0);
-    PCMSK0 = (1 << PCINT4);
-    PCIFR = (1 << PCIF0);  // Clear its interrupt flag by writing a 1.
+//     // Enable pin-change interrupt on PB4 for left encoder, and disable other
+//     // pin-change interrupts.
+//     PCICR = (1 << PCIE0);
+//     PCMSK0 = (1 << PCINT4);
+//     PCIFR = (1 << PCIF0);  // Clear its interrupt flag by writing a 1.
       /*
        * ICP1はアナログコンパレータと機能を兼用しているので
        * それをDISABLEとする。
@@ -110,13 +112,13 @@ void ZumoShieldEncoders::init2()
     // Initialize the variables.  It's good to do this after enabling the
     // interrupts in case the interrupts fired by accident as we were enabling
     // them.
-    lastLeftB = FastGPIO::Pin<LEFT_B>::isInputHigh();
-    lastLeftA = FastGPIO::Pin<LEFT_XOR>::isInputHigh() ^ lastLeftB;
+//     lastLeftB = FastGPIO::Pin<LEFT_B>::isInputHigh();
+//     lastLeftA = FastGPIO::Pin<LEFT_XOR>::isInputHigh() ^ lastLeftB;
     countLeft = 0;
     errorLeft = 0;
 
-    lastRightB = FastGPIO::Pin<RIGHT_B>::isInputHigh();
-    lastRightA = FastGPIO::Pin<RIGHT_XOR>::isInputHigh() ^ lastRightB;
+//     lastRightB = FastGPIO::Pin<RIGHT_B>::isInputHigh();
+//     lastRightA = FastGPIO::Pin<RIGHT_XOR>::isInputHigh() ^ lastRightB;
     countRight = 0;
     errorRight = 0;
 }
